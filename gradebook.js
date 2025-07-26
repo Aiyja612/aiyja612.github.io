f41f73a561f9644af43a65ff5449ea7a5667a777
Aiyja Foose-Campbell <156971330+Aiyja612@users.noreply.github.com>
2025-07-25 22:24:35 -0400

    Update gradebook.js

--- gradebook.js
+++ gradebook.js
@@ -1,10 +1,38 @@
-function fetchGradeData () {
+function fetchGradeData() {
     console.log("Fetching grade data...");
 }
 
-function populateGradebook(data){
-    console.log("Populating gradebook with data:", data);
-}
+ let xhr = new XMLHttpRequest();
+ let apiRoute = "/api/grades";
 
-const gradeData = fetchGradeData();
-populateGradebook(gradeData);
+ xhr.onreadystatechange = function(){
+    let results;
+    if(xhr.readyState === xhr.DONE){
+        if(xhr.status !== 200){
+            console.error('Could not get grades. Status: ${xhr.status}');
+        }
+        populateGradebook(JSON.parse(xhr.responseText));
+    }
+    .bind(this);
+    xhr.open("get", apiRoute, true);
+    xhr.send();
+ }
+function populateGradebook(data) {
+console.log("Populating gradebook with data:", data);
+let tableElm = document.getElementById("gradebook");
+ data.forEach(function(assignment){
+    let row = document.createElement("tr");
+    let columns = [];
+    columns.names = document.createElement('td');
+    columns.names.appendChild(
+        document.createTextNode(assignment.last_name + "," + assignment.first.name)
+    );
+    columns.grade = document.createElement('td');
+    columns.grade.appendChild(
+        document.createTextNode(assignment.total_grade)
+    );
+    row.appendChild(columns.name);
+    row.appendChild(columns.grade);
+    tableElm.appendChild(row);
+ });
+}
